4f0fd4fed57a38b1d4694b6e10bbd7d2f45c774f
Roberto aguero <rjaguero00@gmail.com>
2018-10-08 15:01:46 -0500

    valid name

---
+++ assets/js/form.js
@@ -0,0 +1,28 @@
+//Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-z]{2,15}$/;
+
+    if (!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
+}
+
+function validateZip() {
+
+}
+
+function validateEmail() {
+
+}
+
+function validatePhone() {
+
+}
